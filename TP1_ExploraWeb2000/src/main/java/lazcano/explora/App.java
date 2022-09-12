c82a8f626542b3e8e326348186c72bff04eff171
Alexis Lazcano <0370102@cegepmontpetit.ca>
2022-09-12 14:58:36 -0400

    Création du projet JAVA

---
+++ TP1_ExploraWeb2000/src/main/java/lazcano/explora/App.java
@@ -0,0 +1,13 @@
+package lazcano.explora;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TP1_ExploraWeb2000/src/main/java/lazcano/explora/ExploraLazcano2000.java
@@ -0,0 +1,10 @@
+package lazcano.explora;
+
+public class ExploraLazcano2000 {
+
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
