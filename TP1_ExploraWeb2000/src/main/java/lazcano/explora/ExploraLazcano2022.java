de2d4b7e887247199f2f3c4fe468f7bc8f87116b
Alexis Lazcano <0370102@cegepmontpetit.ca>
2022-09-19 15:34:32 -0400

    FCT: EXPLORATION afficher titre et nombre de liens de la page

--- TP1_ExploraWeb2000/src/main/java/lazcano/explora/ExploraLazcano2022.java
+++ TP1_ExploraWeb2000/src/main/java/lazcano/explora/ExploraLazcano2022.java
@@ -9,8 +9,7 @@ import java.net.URL;
 
 public class ExploraLazcano2022 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
         //COMPLETED 01: Si l'utilisateur ne fournit pas 2 arguments, on lui indique clairement comment utiliser l'application puis l'application s'arrête.
         if(args.length < 2){
 
@@ -53,5 +52,33 @@ public class ExploraLazcano2022 {
             }
         }
 
+        //COMPLETED 05: Si les arguments sont corrects, il faut alors commencer l'exploration de la première page. Tu dois afficher dans la console le titre (title) de la page et l'URL complète.
+        System.out.println("Titre : " + JsoupGet.titreDeLaPage(args[0]) + "URL : " + args[0] + " Liens " + JsoupGet.nbrDeLiens(args[0]));
+//
+        //TODO 06: L'exploration devrait continuer en explorant le premier lien de chaque page. L'exploration s'arrête si une page ne contient aucun lien.
+
+        //TODO 07: L'exploration ignore un lien s'il n'est pas valide.
+
+        //TODO 08: On veut pouvoir contrôler l'exploration en forçant notre application à ignorer certains liens.
+        // Si le premier lien d'une page a une URL qui contient un des mots clés passés en argument, on veut sauter ce lien et passer au suivant jusqu'à trouver un lien ne contenant aucun des mots clés.
+        // Si l'URL pointe vers la page actuelle (URL interne), on devrait ignorer le lien et passer au suivant.
+
+        //TODO 09: Afin d'éviter des boucles infinies, l'exploration doit s'arrêter si le programme rencontre une URL déjà explorée. Il faut afficher un message significatif.
+
+        //TODO 10: Chaque page html explorée doit être stockée dans un fichier avec le même nom que la resource explorée (index.html, 1.html etc.).
+
+        //TODO 11: Chaque apparition d'une adresse courriel dans le contenu d'une page doit être remplacée par prenom.nom@pipo.org avant l'écriture sur le disque.
+
+        //TODO 12: Les liens relatifs entre pages téléchargées doivent être fonctionnels et doivent pointer vers les fichiers locaux si 2 pages sont dans le même dossier.
+
+        //TODO 13: Les liens relatifs doivent fonctionner entre resources locales dans des dossiers différents.
+
+        //TODO 14: Le programme collecte les adresses courriels valides contenues dans chaque page pendant l'exploration. L'application doit sauvegarder la liste des courriels en minuscule dans un fichier dossier_du_projet/resultat/courriels.txt
+
+        //TODO 15: Dans ce fichier, tu dois avoir chaque courriel sur une ligne séparée, classés dans l'ordre alphabétique. Si un courriel a été trouvé plusieurs fois dans l'exploration, tu dois supprimer les doublons.
+
     }
+
+
+
 }
---
+++ TP1_ExploraWeb2000/src/main/java/lazcano/explora/JsoupGet.java
@@ -0,0 +1,41 @@
+package lazcano.explora;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class JsoupGet {
+
+    public static void main(String args[]) throws IOException {
+        //Tests
+        String url = "https://info.cegepmontpetit.ca/3N5-Prog3/phase1.html";
+        System.out.println("Titre: " + titreDeLaPage(url) + " liens " + nbrDeLiens(url));
+    }
+
+    public static String titreDeLaPage(String url) throws IOException {
+        //Get Document object after parsing the html from given url.
+        Document document = Jsoup.connect(url).get();
+
+        //Get title from document object.
+        String title = document.title();
+        return title;
+    }
+
+    public static Integer nbrDeLiens(String url) throws IOException {
+        //Get Document object after parsing the html from given url.
+        Document document = Jsoup.connect(url).get();
+
+        //Get links from document
+        Elements links = document.select("a[href]");
+
+        //Count numbers of Links
+        int i = 0;
+        for (Element link: links) {
+            i++;
+        }
+        return i;
+    }
+}
